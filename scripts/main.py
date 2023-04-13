e59a09c44bddeb3d120fcc20cba957f4660bea08
Syrup <goodgamersz665@gmail.com>
2023-04-13 19:39:08 +0700

    Update main.py
    change the OpenAI API Key link to the latest one

--- scripts/main.py
+++ scripts/main.py
@@ -27,7 +27,7 @@ def check_openai_api_key():
             Fore.RED +
             "Please set your OpenAI API key in config.py or as an environment variable."
         )
-        print("You can get your key from https://beta.openai.com/account/api-keys")
+        print("You can get your key from https://platform.openai.com/account/api-keys")
         exit(1)
 
 def attempt_to_fix_json_by_finding_outermost_brackets(json_string):
